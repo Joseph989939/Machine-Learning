202d376cb5d1ae3ce0f40260a3c358853f6aa45c
Yu-Che Shih <150924140+Joseph989939@users.noreply.github.com>
2024-12-29 22:38:01 +0800

    Add files via upload

---
+++ 爛番茄影評AI評價系統/interactive.py
@@ -0,0 +1,25 @@
+"""
+File: interactive.py
+Name: 
+------------------------
+This file uses the function interactivePrompt
+from util.py to predict the reviews input by 
+users on Console. Remember to read the weights
+and build a Dict[str: float]
+"""
+
+from submission import extractWordFeatures
+from util import interactivePrompt
+
+
+def main():
+	weights = {}
+	with open('weights', 'r', encoding="utf-8") as f:
+		for line in f:
+			key, value = line.split()
+			weights[key] = float(value)
+	interactivePrompt(extractWordFeatures, weights)
+
+
+if __name__ == '__main__':
+	main()
